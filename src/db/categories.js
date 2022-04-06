371253dd5259d6dac902be51165d62f20d59c0d7
Anurag Sharma <anuragsharma0711@gmail.com>
2022-04-06 22:19:19 +0530

    added : rules and categories to db

---
+++ src/db/categories.js
@@ -0,0 +1,60 @@
+const categories = [
+  {
+    title: "Books",
+    categoryNumber: 10,
+    difficulty: "medium",
+    imageURL:
+      "https://raw.githubusercontent.com/ianuragsharma/hostedIMG/main/quiz/categories/Books.png",
+  },
+  {
+    title: "Films",
+    categoryNumber: 11,
+    difficulty: "easy",
+    imageURL:
+      "https://raw.githubusercontent.com/ianuragsharma/hostedIMG/main/quiz/categories/Films.png",
+  },
+  {
+    title: "Music",
+    categoryNumber: 12,
+    difficulty: "hard",
+    imageURL:
+      "https://raw.githubusercontent.com/ianuragsharma/hostedIMG/main/quiz/categories/Music.png",
+  },
+  {
+    title: "Computer",
+    categoryNumber: 18,
+    difficulty: "easy",
+    imageURL:
+      "https://raw.githubusercontent.com/ianuragsharma/hostedIMG/main/quiz/categories/computer.png",
+  },
+  {
+    title: "Mathematics",
+    categoryNumber: 19,
+    difficulty: "easy",
+    imageURL:
+      "https://raw.githubusercontent.com/ianuragsharma/hostedIMG/main/quiz/categories/mathematics.png",
+  },
+  {
+    title: "Animals",
+    categoryNumber: 27,
+    difficulty: "easy",
+    imageURL:
+      "https://raw.githubusercontent.com/ianuragsharma/hostedIMG/main/quiz/categories/Animals.png",
+  },
+  {
+    title: "Vehicles",
+    categoryNumber: 28,
+    difficulty: "medium",
+    imageURL:
+      "https://raw.githubusercontent.com/ianuragsharma/hostedIMG/main/quiz/categories/Vehicles.png",
+  },
+  {
+    title: "Video Games",
+    categoryNumber: 15,
+    difficulty: "medium",
+    imageURL:
+      "https://raw.githubusercontent.com/ianuragsharma/hostedIMG/main/quiz/categories/video-games.png",
+  },
+];
+
+export { categories };
---
+++ src/db/rules.js
@@ -0,0 +1,23 @@
+const rules = [
+  {
+    id: 1,
+    rule: "There are 10 questions for each category.",
+  },
+  {
+    id: 2,
+    rule: "Each correct answer grants you 10 points.",
+  },
+  {
+    id: 3,
+    rule: "No questions can be skipped.",
+  },
+  {
+    id: 4,
+    rule: "There are no deductions for wrong answers.",
+  },
+  {
+    id: 5,
+    rule: "To go to next question, click on Next",
+  },
+];
+export { rules };
