9b4bdbb3c27d2c96a877f8e5f2afae6948ada474
Anurag Sharma <anuragsharma0711@gmail.com>
2022-04-09 17:31:09 +0530

    added exports and exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { Navbar } from "./Navbar/Navbar";
 export { CategoriesCard } from "./CategoriesCard/CategoriesCard";
+export { Question } from "./Question/Question";
+export { Loader } from "./Loader/Loader";
