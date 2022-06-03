05b65954f5e766b93375514aa191528657eae03c
Anurag Sharma <anuragsharma0711@gmail.com>
2022-06-03 10:39:03 +0530

    feat:added env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyA8lkScEAUnQXuyAm4o-_1wNF6jRDmrh90",
-  authDomain: "quizzaap.firebaseapp.com",
-  projectId: "quizzaap",
-  storageBucket: "quizzaap.appspot.com",
-  messagingSenderId: "285020405393",
-  appId: "1:285020405393:web:2fc6455796b981daa48930",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
